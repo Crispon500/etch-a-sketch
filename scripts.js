139e9c2dac203f6e969995b056ff70c1660421fd
KJT <crispon500@gmail.com>
2023-03-13 19:37:26 -0500

    set color to variable values

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
 //variable with reference to the grid container
 const gridContainer = document.getElementById('grid-container');
-const gridButton = document.querySelector('#create-grid');
+
+//create buttons
+const resetButton = document.querySelector('#reset-grid');
+const eraserButton = document.querySelector('#erasorButton');
+
+//default grid size and color
 let size = 50;
+let color = "black";
+
 
 //gives the button an onclick function to initiate the grid creation
-gridButton.onclick = () => createGrid(size);
+resetButton.onclick = () => createGrid(size);
 
 //this function lays out and fills the grid with divs
 function createGrid(size) {
   deleteGrid();
   gridLayout(size, size);
-  fillGrid(size, "lavender");
+  fillGrid(size);
 }
 
 //this function will apply a grid layout
@@ -20,13 +27,13 @@ function gridLayout(height, width) {
 }
 
 //this function will fill a grid with divs
-const fillGrid = (size, color) => {
+const fillGrid = (size) => {
   const numOfBlocks = size*size;
   for (let i=0; i<numOfBlocks; i++){
     let newDiv = document.createElement("div");
     newDiv.id = `card ${i}`;
     gridContainer.appendChild(newDiv);
-    newDiv.onmouseover = () => {newDiv.style.backgroundColor = "black"};
+    newDiv.onmouseover = () => {newDiv.style.backgroundColor = color};
     };
 }
 
